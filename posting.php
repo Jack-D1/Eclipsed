6c56d75afce28976e45f5e9e600d813b5ec7aff8
Jack Donnelly <jackr_donnelly00@yahoo.com>
2018-02-06 09:30:41 +0000

    Delete posting.php

--- posting.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_start();
-	include("connection.php");
-    $text = $_POST['text'];
-	$UID = $_SESSION['UserID'];
-	$text = mysqli_real_escape_string($connection, $text);
-	$SendPost = mysqli_query($connection, "INSERT INTO post(postedBy, Post) VALUES ('$UID', '$text')");
-	mysqli_close($connection);
-?>
