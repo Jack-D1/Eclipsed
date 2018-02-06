583868b95a4395324889431a57bb6725768b53b5
Jack Donnelly <jackr_donnelly00@yahoo.com>
2018-02-06 09:25:20 +0000

    Sorted Files into folders
    Will probably need to fix linking

---
+++ scripts/account/follow.php
@@ -0,0 +1,16 @@
+<?php
+include ("/scripts/validation/connection.php");
+$action = $_POST['action'];
+$follower = $_POST['follower'];
+$user = $_POST['userID'];
+
+if($action == "Follow"){
+	mysqli_query($connection, "INSERT INTO following VALUES ('$follower', '$user')");
+	exit();
+}else{
+	mysqli_query($connection, "DELETE from following WHERE UserID1 = '$follower' AND UserID2 = '$user'");
+	exit();
+}
+
+
+?>
