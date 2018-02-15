e6242b6851988c4d1fe73f01892a1ec9320c63a5
Jack Donnelly <jackr_donnelly00@yahoo.com>
2018-02-15 14:40:30 +0000

    Input sanitisation

--- scripts/post/posting.php
+++ scripts/post/posting.php
@@ -4,6 +4,7 @@
     $text = $_POST['text'];
 	$UID = $_SESSION['UserID'];
 	$text = mysqli_real_escape_string($connection, $text);
+	$text = htmlspecialchars($text);
 	$SendPost = mysqli_query($connection, "INSERT INTO post(postedBy, Post) VALUES ('$UID', '$text')");
 	mysqli_close($connection);
 ?>
